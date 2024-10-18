bdeed56acc5c446983889f1a15927a015eb2cea0
hank.chen <hank.chen@baifu-tech.net>
2024-10-18 18:42:18 +0800

    @移除最後的seelog，修復錯誤@

--- internal/pkg/web/gin/middleware/router/respRecover.go
+++ internal/pkg/web/gin/middleware/router/respRecover.go
@@ -2,15 +2,15 @@ package router
 
 import (
 	"beautyProject/internal/pkg/web/gin/handler/response"
-	log "github.com/cihub/seelog"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"runtime/debug"
 )
 
 func respRecover(c *gin.Context) {
 	if err := recover(); err != nil {
 		log.Infof("Recovered from panic: %v\n", err)
-		log.Criticalf("Stack trace:\n%s", debug.Stack())
+		log.Errorf("Stack trace:\n%s", debug.Stack())
 		response.Panic(c)
 	}
 }
