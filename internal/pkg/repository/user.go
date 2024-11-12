5d0ff71f3dd2da8e70ee8cd5aaaefa31809c7598
hank.chen <hank.chen@baifu-tech.net>
2024-11-12 18:11:43 +0800

    @user 整理@

--- internal/pkg/repository/user.go
+++ internal/pkg/repository/user.go
@@ -1,24 +1,28 @@
 package repository
 
 import (
-	"beautyProject/internal/pkg/db/sql"
 	"beautyProject/internal/pkg/model"
-	"beautyProject/internal/pkg/web/request"
+	"beautyProject/internal/pkg/util/db/sql"
 )
 
-type User struct{}
-
-func (u *User) Find(name string) model.User {
-	user := model.User{Name: name}
-	result := sql.Db.First(&user)
-	return result
+type User struct {
 }
 
-func (u *User) Add(req request.UserReq) {
-	user := model.User{Name: req.UserName, Password: req.UserPassword, AuthorizationLevel: "user"}
-	sql.Db.Create(&user)
+func (u *User) FindByName(name string) (*model.User, bool) {
+	user := &model.User{}
+	result := sql.Db.Where("name = ?", name).Find(user)
+	if result.RowsAffected == 0 {
+		//log.Info("No user found with name:", name)
+		return nil, false
+	}
+	//log.Info("User found")
+	return user, true
 }
 
-func (u *User) Delete(id int) {
+func (u *User) Add(user *model.User) error {
+	return sql.Db.Create(user).Error
+}
+
+func (u *User) Remove(id int) {
 	sql.Db.Delete(&model.User{}, id)
 }
--- internal/services/backend/user/user.go
+++ internal/services/backend/user/user.go
@@ -2,22 +2,38 @@ package user
 
 import (
 	"beautyProject/internal/pkg/dto"
+	"beautyProject/internal/pkg/model"
 	"beautyProject/internal/pkg/repository"
+	"beautyProject/internal/pkg/util/str"
+	"beautyProject/internal/pkg/util/web"
 	"beautyProject/internal/pkg/web/request"
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 type User struct {
 }
 
-func (u *User) Register(req request.UserReq) dto.Msg {
+func (u *User) Register(c *gin.Context, userRepo repository.User, req request.UserReq) dto.Msg {
 	//panic("no value for $USER")
-	userRepo := repository.User{}
-	//userRepo.Add(req)
-	//userRepo.Delete(1)
-	user := userRepo.Find(req.UserName)
-	if user.RowsAffected != 0 {
-
+	user, isFind := userRepo.FindByName(req.UserName)
+	if isFind == true {
+		msg := dto.Msg{Success: false, Message: "用戶名被註冊"}
+		log.Infof("%v", msg)
+		return msg
+	}
+	user = &model.User{Name: req.UserName, Password: str.HashPassword(req.UserPassword), AuthorizationLevel: "user"}
+	err := userRepo.Add(user)
+	if err != nil {
+		msg := dto.Msg{Success: false, Message: "註冊失敗"}
+		log.Infof("%v", msg)
+		return msg
+	}
+	err = web.SetJwtCookie(c, user.ID)
+	if err != nil {
+		msg := dto.Msg{Success: false, Message: "設定cookie失敗"}
+		log.Infof("%v", msg)
+		return msg
 	}
 	msg := dto.Msg{Success: true, Message: "註冊成功"}
 	log.Infof("%v", msg)
@@ -25,10 +41,32 @@ func (u *User) Register(req request.UserReq) dto.Msg {
 
 }
 
-func (u *User) Login(req request.UserReq) {
-
+func (u *User) Login(c *gin.Context, userRepo repository.User, req request.UserReq) dto.Msg {
+	user, isFind := userRepo.FindByName(req.UserName)
+	if isFind == false {
+		msg := dto.Msg{Success: false, Message: "用戶未註冊"}
+		log.Infof("%v", msg)
+		return msg
+	}
+	if user.Password != str.HashPassword(req.UserPassword) {
+		msg := dto.Msg{Success: false, Message: "密碼錯誤"}
+		log.Infof("%v", msg)
+		return msg
+	}
+	err := web.SetJwtCookie(c, user.ID)
+	if err != nil {
+		msg := dto.Msg{Success: false, Message: "設定cookie失敗"}
+		log.Infof("%v", msg)
+		return msg
+	}
+	msg := dto.Msg{Success: true, Message: "登入成功"}
+	log.Infof("%v", msg)
+	return msg
 }
 
-func (u *User) Logout() {
-
+func (u *User) Logout(c *gin.Context) dto.Msg {
+	web.UnsetJwtCookie(c)
+	msg := dto.Msg{Success: true, Message: "登出成功"}
+	log.Infof("%v", msg)
+	return msg
 }
