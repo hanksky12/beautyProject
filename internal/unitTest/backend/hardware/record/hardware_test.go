5410dcf272ae231b592227c872262fd0ac4c426b
hank.chen <hank.chen@baifu-tech.net>
2025-02-10 14:48:54 +0800

    @單元測試@
    1.user
    2.hardware

---
+++ internal/unitTest/backend/hardware/record/hardware_test.go
@@ -0,0 +1,73 @@
+package record
+
+import (
+	"beautyProject/internal/pkg/mocks"
+	"beautyProject/internal/services/backend/hardware/record/base"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type Suite struct {
+	suite.Suite
+	userId       string
+	mockHardware *mocks.IWorkAndName
+}
+
+func (suite *Suite) SetupSuite() {
+	suite.userId = "1"
+	suite.mockHardware = new(mocks.IWorkAndName)
+	suite.mockHardware.On("Work", "1")
+	suite.mockHardware.On("Name").Return("mock")
+
+}
+
+func TestStart(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
+
+func (suite *Suite) TestRecorder_Success_Start() {
+	log.Info("TestRecorder_Success_Start")
+	recorder := base.NewRecorder(suite.mockHardware, suite.userId)
+	recorder.Initialize()
+
+	msgDto := recorder.Start()
+	suite.Equal(true, msgDto.Success)
+	suite.Equal("開始記錄", msgDto.Message)
+	recorder.Stop()
+}
+
+func (suite *Suite) TestRecorder_Already_Start() {
+	log.Info("TestRecorder_Already_Start")
+	recorder := base.NewRecorder(suite.mockHardware, suite.userId)
+	recorder.Initialize()
+
+	_ = recorder.Start()
+	msgDto := recorder.Start()
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("已在記錄中...", msgDto.Message)
+	recorder.Stop()
+}
+
+func (suite *Suite) TestRecorder_Not_Start() {
+	log.Info("TestRecorder_Not_Start")
+	recorder := base.NewRecorder(suite.mockHardware, suite.userId)
+	recorder.Initialize()
+
+	msgDto := recorder.Stop()
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("沒有在記錄...", msgDto.Message)
+}
+
+func (suite *Suite) TestRecorder_Success_Stop() {
+	log.Info("TestRecorder_Success_Stop")
+	recorder := base.NewRecorder(suite.mockHardware, suite.userId)
+	recorder.Initialize()
+
+	_ = recorder.Start()
+	msgDto := recorder.Stop()
+
+	suite.Equal(true, msgDto.Success)
+	suite.Equal("停止記錄", msgDto.Message)
+}
---
+++ internal/unitTest/backend/user/user_test.go
@@ -0,0 +1,162 @@
+package record
+
+import (
+	"beautyProject/internal/pkg/mocks"
+	"beautyProject/internal/pkg/model"
+	"beautyProject/internal/pkg/util/str"
+	"beautyProject/internal/pkg/web/request"
+	"beautyProject/internal/services/backend/user"
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type Suite struct {
+	suite.Suite
+	MockUserRepo *mocks.IRepoUser
+	MockJwt      *mocks.ISetCookie
+	UserReq      request.UserReq
+	ModelUser    *model.User
+}
+
+func (suite *Suite) SetupTest() {
+	suite.MockUserRepo = new(mocks.IRepoUser)
+	suite.MockJwt = new(mocks.ISetCookie)
+	suite.UserReq = request.UserReq{
+		UserName:     "test",
+		UserPassword: "test",
+	}
+	suite.ModelUser = &model.User{
+		Name:               suite.UserReq.UserName,
+		Password:           str.HashPassword(suite.UserReq.UserPassword),
+		AuthorizationLevel: "user",
+	}
+}
+
+func TestStart(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
+
+func (suite *Suite) TestUser_Login_Success() {
+	log.Info("TestUser_Login_Success")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(suite.ModelUser, true)
+	suite.MockUserRepo.On("Add", suite.ModelUser).
+		Return(nil)
+	suite.MockJwt.On("SetCookie", uint(0)).Return(nil)
+
+	userServ := user.User{}
+	msgDto := userServ.Login(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(true, msgDto.Success)
+	suite.Equal("登入成功", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Login_Not_Register() {
+	log.Info("TestUser_Login_Not_Register")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(suite.ModelUser, false)
+	//suite.MockUserRepo.On("Add", suite.ModelUser).
+	//	Return(nil)
+	//suite.MockJwt.On("SetCookie", uint(0)).Return(nil)
+
+	userServ := user.User{}
+	msgDto := userServ.Login(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("用戶未註冊", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Login_Wrong_Password() {
+	log.Info("TestUser_Login_Wrong_Password")
+	modelUserWrong := &model.User{
+		Name:               suite.UserReq.UserName,
+		Password:           str.HashPassword("test1"),
+		AuthorizationLevel: "user",
+	}
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(modelUserWrong, true)
+	//suite.MockUserRepo.On("Add", suite.ModelUser).
+	//	Return(nil)
+	//suite.MockJwt.On("SetCookie", uint(0)).Return(nil)
+
+	userServ := user.User{}
+	msgDto := userServ.Login(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("密碼錯誤", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Login_SetCookie_Failed() {
+	log.Info("TestUser_Login_SetCookie_Failed")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(suite.ModelUser, true)
+	suite.MockUserRepo.On("Add", suite.ModelUser).
+		Return(nil)
+	suite.MockJwt.On("SetCookie", uint(0)).Return(errors.New("cookie error"))
+
+	userServ := user.User{}
+	msgDto := userServ.Login(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("設定cookie失敗", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Register_Success() {
+	log.Info("TestUser_Register_Success")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(nil, false)
+	suite.MockUserRepo.On("Add", suite.ModelUser).
+		Return(nil)
+	suite.MockJwt.On("SetCookie", uint(0)).Return(nil)
+
+	userServ := user.User{}
+	msgDto := userServ.Register(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(true, msgDto.Success)
+	suite.Equal("註冊成功", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Register_Repeat() {
+	log.Info("TestUser_Register_Repeat")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(nil, true)
+	//suite.MockUserRepo.On("Add", suite.ModelUser).
+	//	Return(nil)
+	//suite.MockJwt.On("SetCookie", uint(0)).Return(nil)
+	userServ := user.User{}
+	msgDto := userServ.Register(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("用戶名被註冊", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Register_Sql_Insert_Failed() {
+	log.Info("TestUser_Register_Sql_Insert_Failed")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(nil, false)
+	suite.MockUserRepo.On("Add", suite.ModelUser).
+		Return(errors.New("add error"))
+	//suite.MockJwt.On("SetCookie", uint(0)).Return(nil)
+	userServ := user.User{}
+	msgDto := userServ.Register(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("註冊失敗", msgDto.Message)
+}
+
+func (suite *Suite) TestUser_Register_SetCookie_Failed() {
+	log.Info("TestUser_Register_SetCookie_Failed")
+	suite.MockUserRepo.On("FindByName", suite.UserReq.UserName).
+		Return(nil, false)
+	suite.MockUserRepo.On("Add", suite.ModelUser).
+		Return(nil)
+	suite.MockJwt.On("SetCookie", uint(0)).Return(errors.New("cookie error")).Once()
+
+	userServ := user.User{}
+	msgDto := userServ.Register(suite.MockJwt, suite.MockUserRepo, suite.UserReq)
+
+	suite.Equal(false, msgDto.Success)
+	suite.Equal("設定cookie失敗", msgDto.Message)
+}
