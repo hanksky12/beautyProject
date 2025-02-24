656f5cad7bda9e513fe5ff8a8af3541d51794e45
hank.chen <hank.chen@baifu-tech.net>
2025-02-24 17:48:49 +0800

    @前端@form 增加基礎件@

---
+++ frontend/src/components/form/column/MinXColumn.vue
@@ -0,0 +1,47 @@
+<template>
+  <input-column  :english_name='english_name'
+                 :chinese_name='chinese_name'
+                 :rules="rules"
+                 :values="values"
+                 :disabled="disabled"
+                 :type="type"
+                 :min="min"
+                  :max="max"
+                 :step="step"
+  />
+</template>
+
+<script>
+import InputColumn from "@/components/form/column/base/InputColumn.vue";
+
+export default {
+  components: {
+      'input-column': InputColumn
+  },
+  props:{
+    rules: {
+      type: String,
+      default: ""
+    },
+    values: {
+          type: String,
+          default: null,
+    },
+    disabled: {
+          type: Boolean,
+          default: false,
+    },
+  },
+  data() {
+    return {
+        english_name: 'min_x',
+        chinese_name: '最小X座標',
+        type:"number",
+        min: 0,
+      max: 999999,
+        step: 1
+    }
+  },
+};
+</script>
+
--- frontend/src/components/form/column/MouseActionNameColumn.vue
+++ frontend/src/components/form/column/MouseActionNameColumn.vue
@@ -1,21 +1,21 @@
 <template>
   <select-column  :english_name='english_name'
-                 :chinese_name='chinese_name'
+                  :chinese_name='chinese_name'
                   :options_array='options_array'
-                 :rules="rules"
-                 :values="values"
+                  :rules="rules"
+                  :values="values"
                   :disabled="disabled"
   />
 </template>
 <script>
 import SelectColumn from "@/components/form/column/base/SelectColumn.vue";
-import { GetHardwareNameApiMixin } from "@/mixins/api/server/hardware/getName";
+import {GetMouseActionNameApiMixin} from "@/mixins/api/user/mouse/getName";
 
 
 export default {
-  mixins: [GetHardwareNameApiMixin],
+  mixins: [GetMouseActionNameApiMixin],
   components: {
-      'select-column': SelectColumn
+    'select-column': SelectColumn
   },
   props:{
     rules: {
@@ -33,18 +33,18 @@ export default {
   },
   data() {
     return {
-        english_name: 'hardware_name',
-        chinese_name: '硬體名稱',
+        english_name: 'mouse_action_name',
+        chinese_name: '滑鼠操作名稱',
         options_array: [{ text: '未选择', value: null }],
     }
   },
   async created() {
     try {
-      const response = await this.getHardwareNameApi();
+      const response = await this.getMouseActionNameApi();
       console.log("create response:", response);
       this.options_array = [{ text: '未选择', value: null }, ...response.data.map(item => ({
-        text: item.hardware_name, // 使用 hardware_name 作為顯示的文字
-        value: item.hardware_name  // 使用 hardware_name 作為選項的值
+        text: item.mouse_action_name,
+        value: item.mouse_action_name
       }))];    } catch (error) {
       console.error("Error fetching hardware names:", error);
     }
