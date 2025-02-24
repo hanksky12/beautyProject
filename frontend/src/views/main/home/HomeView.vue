b6f15b0826986ceea2ba684276b4ac7c9ef0bce7
hank.chen <hank.chen@baifu-tech.net>
2025-02-24 17:48:23 +0800

    @前端@首頁 呈現硬體資訊@

--- frontend/src/views/main/home/HomeView.vue
+++ frontend/src/views/main/home/HomeView.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <bread-crumb :props_items="items"/>
+        <query-table />
         <br>
     </div>
 </template>
 
 <script>
 import Breadcrumb from '@/components/Breadcrumb.vue'
+import QueryTable from './components/QueryTable.vue';
+
 export default {
     components: {
         'bread-crumb':Breadcrumb,
+      'query-table':QueryTable,
     },
     data() {
       return {
---
+++ frontend/src/views/main/home/components/QueryTable.vue
@@ -0,0 +1,50 @@
+<template>
+    <base-table
+        :columns="columns"
+        :api_path="api_path"
+        :row_style="rowStyle"
+    />
+
+</template>
+
+<script>
+import BaseTable from "@/components/BaseTable.vue";
+
+export default {
+  components: {
+    'base-table':BaseTable,
+  },
+  data () {
+    return {
+      api_path: "/server/hardware-specs/info",
+      columns: [
+                {
+                  field: 'cpu_info',
+                  title: 'CPU規格',
+                },
+                {
+                  field: 'memory_info',
+                  title: '記憶體規格',
+                },
+                {
+                  field: 'disk_info',
+                  title: '硬碟規格',
+                },
+                {
+                  field: 'host_info',
+                  title: '主機資訊',
+                },
+      ],
+      rowStyle() {
+          return { classes: "bg-type-yellow" }
+    },
+    }
+  },
+}
+</script>
+<style>
+.responsive-image {
+  max-width: 70%; /* 圖片最大寬度為父容器寬度 */
+  height: auto; /* 保持原始高度寬高比 */
+}
+</style>
